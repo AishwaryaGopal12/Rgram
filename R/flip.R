f0e8925888113b1460bd3ac157859b9873de41f4
jla510 <jla510@student.ubc.ca>
2021-03-06 02:46:31 -0800

    Added function specification

---
+++ R/flip.R
@@ -0,0 +1,14 @@
+#' Flipping an image in either horizontal or vertical direction
+#'
+#' @param image path of input file
+#' @param direction direction of flip, horizontal="h", vertical = "v"
+#' @param output_path string, path for the output image file
+#'
+#' @return an image file at the specified output path in .png format
+#' @export
+#'
+#' @examples
+#' flip("images/samples.jpg", 'h' , "images/flip.png")
+flip <- function(image, direction, output_path){
+  break
+}
