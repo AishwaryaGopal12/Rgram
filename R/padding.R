1dbc67bced9dad4ac66659aec577cf8fda83655c
Rui Wang <rwang778@student.ubc.ca>
2021-03-12 23:03:40 -0800

    Update Padding test and R code

--- R/padding.R
+++ R/padding.R
@@ -8,16 +8,15 @@ library(testit)
 #' @param width the pixels of the padding, the padding width for left, right, top, bottom are the same
 #' @param output_path string, path for the output image file
 #'
-#' @return an image file at the specified output path in .png format
+#' @return an image matrix that has been padded
 #' @export
 #'
 #' @examples
 #' pad("images/samples.jpg", 20, "images/padding.png")
-padding <- function(input_path, width, output_path){
+padding <- function(input_path, width){
 
   # exception handling
   assert("Please provide a string as the path for the input image file.", is.character(input_path))
-  assert("Please provide a string as the path for the output image file.", is.character(output_path))
 
   #Reading image file as matrix
   input <- load.image(input_path)
--- tests/testthat/test_padding.R
+++ tests/testthat/test_padding.R
@@ -17,7 +17,13 @@ test_that("If user specifies an additional argument, it throws an error", {
 
 })
 
+test_that("Color image is paddingred", {
 
+  output<- padding("test_images/padding/sample.jpg", 20)
+  exp_output <- load.image("test_images/padding/padding_output.jpg")
+  expect_equal(output, exp_output, tolerance=1e-1)
+
+})
 
 test_that("In case the input/output is not a string", {
 
@@ -25,12 +31,11 @@ test_that("In case the input/output is not a string", {
     expect_error(padding("test_images/padding/input.jpg", 20, c(1,2,3)))
 })
 
-test_that("In case the input/output path does not exist", {
+test_that("In case the input path does not exist", {
 
-    expect_error(padding("123/padding/input.jpg", 20, "test_images/padding/padding.jpg"))
-    expect_error(padding("test_images/padding/input.jpg", 20, "123/padding/padding.jpg"))
+    expect_error(padding("123/padding/input.jpg", 20))
 })
 
 test_that("In case the input is not an image", {
-    expect_error(padding("test_images/padding/test.pdf", 20, "test_images/padding/padding.jpg"))
+    expect_error(padding("test_images/padding/test.pdf", 20))
 })
