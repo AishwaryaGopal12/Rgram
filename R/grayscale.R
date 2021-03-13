04200fe26ae4a5eb9b60bab23c190e8270e20aaf
Aishwarya Gopal <aishwaryagopal12@gmail.com>
2021-03-12 20:41:22 -0800

    Added grayscale function

--- R/grayscale.R
+++ R/grayscale.R
@@ -7,7 +7,33 @@
 #' @export
 #'
 #' @examples
-#' grayscale("images/samples.jpg", "images/greyscale.png")
+#' grayscale("images/sample.png", "images/greyscale.png")
 grayscale <- function(image, output_path){
-  break
+    # exception handling
+    assert("Please type in  a string as the path for the input image file.", is.character(image))
+    assert("Please type in  a string as the path for the output image file.", is.character(output_path))
+
+    input_img <- readPNG(image)
+
+    input_img_height <- dim(input_img)[1]
+    input_img_width <- dim(input_img)[2]
+
+    img_gs <- array(dim = dim(input_img))
+
+    for (i in 1:input_img_height) {
+      for (j in 1:input_img_width) {
+        R = input_img[i,j,1]
+        G = input_img[i,j,2]
+        B = input_img[i,j,3]
+
+        gray_scale = 0.2989*R + 0.5870*G + 0.1140*B
+
+        output_img[i,j,1] = gray_scale
+        output_img[i,j,2] = gray_scale
+        output_img[i,j,3] = gray_scale
+      }
+    }
+
+    writePNG(output_img, output_path)
+
 }
