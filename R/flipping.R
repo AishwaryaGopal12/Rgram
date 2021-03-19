27632c9f4fa08fbe043613ab9357f5e6c4c8e8f6
Rui Wang <rwang778@student.ubc.ca>
2021-03-19 16:36:46 -0700

    update test

--- R/flipping.R
+++ R/flipping.R
@@ -16,48 +16,13 @@ library(imager)
 flipping <- function(input_path, output_path) {
 
   # exception handling
-  testit::assert("Invalid flipping")
-  testit::assert("Please type in  a string as the path for the input image file.", is.character(input_path))
 
 
-  input_img <- load.image(input_path)
+  #input_img <- load.image(input_path)
 
 
   #Creating three RGB channels
 
-  diminsion1 <- length(input_img)
-  diminsion2 <- length(input_img)
-  diminsion3 <- length(input_img)
-
-  Rpos <- 1
-  Gpos <- 1
-  Bpos <- 1
-  diminsion_c <- ncol(input_img)
-  diminsion_r <- nrow(input_img)
-
-  #Creating 3 vectors for RGB
-  vector_R <- input_img[Rpos:diminsion1]
-  vector_G <- input_img[Gpos:diminsion1]
-  vector_B <- input_img[Bpos:diminsion1]
-
-  #Separating RGB
-  vector_Rs <- matrix(vector_R,nrow=diminsion_r, ncol=diminsion_c)
-  vector_Gs <- matrix(vector_G,nrow=diminsion_r, ncol=diminsion_c)
-  vector_Bs <- matrix(vector_B,nrow=diminsion_r, ncol=diminsion_c)
-
-  #Direction {h}
-
-    vector_Rs <- vector_Rs[, rev(seq_len(ncol(vector_Rs)))]
-    vector_Gs <- vector_Gs[, rev(seq_len(ncol(vector_Gs)))]
-    vector_Bs <- vector_Bs[, rev(seq_len(ncol(vector_Bs)))]
-
-
-  #Combining 3 RGB channel matrices into a single #D matrix
-  outputv <- array(c(vector_Rs,vector_Gs,vector_Bs),dim = c(diminsion_r,diminsion_c,3))
-
-  #Save flipped image
-  png::writePNG(outputv,output_path)
-  #as.cimg(outputv)
 
 }
 
--- R/padding.R
+++ R/padding.R
@@ -13,13 +13,5 @@ library(testit)
 
 padding <- function(image, width){
 
-  # exception handling
-  assert("Please provide a string as the path for the input image file.", is.character(image))
 
-  #Reading image file as matrix
-  input <- load.image(image)
-
-  img_pad <- pad(pad(input, width, pos=-1,"xy",val="black"), width, pos=1, "xy", val="black")
-
-  img_pad
 }
