30ca8b773e05b3b57fe080acb1423087f484ece0
Rui Wang <rwang778@student.ubc.ca>
2021-03-20 13:12:31 -0700

    revert Rgram back

--- tests/testthat/test_padding.R
+++ tests/testthat/test_padding.R
@@ -1,8 +1,7 @@
 # This script tests the padding() function
 
 context("Padding image")
-library(imager)
-library(testit)
+library(imageR)
 
 
 test_that("In case the input is not an image", {
@@ -14,28 +13,30 @@ test_that("In case the input is not an image", {
 
 test_that("If user specifies an additional argument, it throws an error", {
 
-    expect_error(padding("test_images/padding/input.jpg", 20, "test_images/padding/padding.jpg", "box"))
+    expect_error(padding("test_images/padding/sample.jpg", 20, "test_images/padding/padding_output.jpg", "box"))
 
 })
 
 test_that("Color image is paddingred", {
+    
+  padding("test_images/padding/input.jpg", 20, "test_images/padding/padding.jpg")
+  output <- load.image("test_images/padding/padding.jpg")
+  expect_equal(output, exp_output, tolerance=1e-5)
 
-  output_p<- padding("test_images/padding/sample.jpg", 20)
-  exp_output_p <- load.image("https://raw.githubusercontent.com/UBC-MDS/Rgram/master/tests/testthat/test_images/padding/padding_output.jpg")
-  expect_equal(output_p, exp_output_p, tolerance=1e-1)
 })
 
 test_that("In case the input/output is not a string", {
-
+    
     expect_error(padding(123, "test_images/padding/padding.jpg"))
     expect_error(padding("test_images/padding/input.jpg", 20, c(1,2,3)))
 })
 
-test_that("In case the input path does not exist", {
-
-    expect_error(padding("123/padding/input.jpg", 20))
+test_that("In case the input/output path does not exist", {
+    
+    expect_error(padding("123/padding/input.jpg", 20, "test_images/padding/padding.jpg"))
+    expect_error(padding("test_images/padding/input.jpg", 20, "123/padding/padding.jpg"))
 })
 
 test_that("In case the input is not an image", {
-    expect_error(padding("test_images/padding/test.pdf", 20))
+    expect_error(padding("test_images/padding/test.pdf", 20, "test_images/padding/padding.jpg"))
 })
